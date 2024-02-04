6a56498bfbebaed097ab9d8dc006a67023eae80c
jjm <mjj0131@gmail.com>
2024-02-04 10:12:16 -0800

    list and navigation

---
+++ Landmark/Landmark/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmark
+//
+//  Created by Jingjing Mao on 2/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
