d01047e53bab5e9132235a5cd6c5b9f6754075bd
jjm <mjj0131@gmail.com>
2024-02-05 20:15:53 -0800

    Handling user input and data binding

--- Landmark/Landmark/Landmarks/LandmarkDetail.swift
+++ Landmark/Landmark/Landmarks/LandmarkDetail.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id})!
+    }
+    
     var body: some View {
         ScrollView {
+            @Bindable var modelData = modelData
+            
             MapView(coordinate: landmark.locationCoordinate).frame(height:300)
+            
             CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
+            
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                   Text(landmark.name)
+                        .font(.title)
+                   FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+               
                 HStack {
                     Text(landmark.park)
                     Spacer()
@@ -41,5 +54,6 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0]).environment(modelData)
 }
