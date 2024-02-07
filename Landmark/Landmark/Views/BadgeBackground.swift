19953186979f9fe496a43a940fad51e4cb14df1b
jjm <mjj0131@gmail.com>
2024-02-06 21:27:29 -0800

    partially finish drawing section

---
+++ Landmark/Landmark/Views/BadgeBackground.swift
@@ -0,0 +1,35 @@
+//
+//  BadgeBackground.swift
+//  Landmark
+//
+//  Created by Jingjing Mao on 2/5/24.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            var width: CGFloat = 100.0
+            let height = width
+            path.move(to: CGPoint(
+                x: width * 0.95,
+                y: height * 0.20
+            )
+            )
+            
+            HexagonParameters.segments.forEach { segment in
+                path.addLine(to: CGPoint(
+                    x: width * segment.line.x,
+                    y: height * segment.line.y
+                  )
+                )
+            }
+        }
+        .fill(.black)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
